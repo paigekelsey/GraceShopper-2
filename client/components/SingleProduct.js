b2c1c81c6255aa21aabe2c0a526d7911607970ed
Sala Yoshida <yoshidasala@gmail.com>
2021-11-11 14:03:26 -0500

    class to functional

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,44 +1,37 @@
 import React from "react";
-import { connect } from "react-redux";
 import { fetchSingleProduct } from "../redux/singleProduct";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
-export class SingleProduct extends React.Component {
-  componentDidMount() {
-    this.props.fetchSingleProduct(this.props.match.params.id);
-  }
-  render() {
-    const product = this.props.product;
-    return (
-      <div>
-        <div className="single-product-view">
-          <div className="right">
-            <h3>Left in stock: {product.inventory}</h3>
-            <h3>Price: ${product.price}</h3>
-            <button className="btn first">Add to Cart</button>
-          </div>
-          <div className="left">
-            <h3>{product.name}</h3>
-            <img src={product.imageUrl} />
-            <p>{product.description}</p>
-          </div>
+const SingleProduct = (props) => {
+  //this is the same as mapstate
+  const product = useSelector((state) => state.product);
+  // this is the same  mapdispatch
+  const dispatch = useDispatch();
+
+  //when we pass in an empty arr it acts as componentdidmount and when we dont pass in a second arg it acts as componentdidupdate
+  useEffect(() => {
+    dispatch(fetchSingleProduct(props.match.params.id));
+    // Safe to add dispatch to the dependencies array
+  }, []);
+
+  return (
+    <div>
+      <div className='single-product-view'>
+        <div className='right'>
+          <h3>Left in stock: {product.inventory}</h3>
+          <h3>Price: ${product.price}</h3>
+          <button className='btn first'>Add to Cart</button>
+        </div>
+        <div className='left'>
+          <h3>{product.name}</h3>
+          <img src={product.imageUrl} />
+          <p>{product.description}</p>
         </div>
       </div>
-    );
-  }
-}
-
-const mapState = (state) => {
-  return {
-    product: state.product,
-  };
+    </div>
+  );
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    fetchSingleProduct: (id) => {
-      dispatch(fetchSingleProduct(id));
-    },
-  };
-};
-
-export default connect(mapState, mapDispatch)(SingleProduct);
+export default SingleProduct;
